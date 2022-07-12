9fd5e208e01159985a563546906adf51c961d588
linanpro <109135523+linanpro@users.noreply.github.com>
2022-07-12 22:10:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask,render_template, url_for, request
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template("index.html")
+#@app.route('/')
+#@app.route('/home')
+#def home():
+#    return render_template("index.html")
 
 @app.route('/result',methods=['POST', 'GET'])
 def result():
     output = request.form.to_dict()
     print(output)
-    name = output["name"]
-    print(name)
-    return render_template('index.html', name = name)
+#    name = output["name"]
+#    print(name)
+    return render_template('index.html')
     
 
 #if __name__ == "__main__":
