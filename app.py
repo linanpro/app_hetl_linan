4369b4c6657bcbeb8af8bb6d75b3c4b0c0e99959
linanpro <109135523+linanpro@users.noreply.github.com>
2022-07-12 20:26:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template, url_for, request
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template("index.html")
+#@app.route('/')
+#@app.route('/home')
+#def home():
+#    return render_template("index.html")
 
 @app.route('/result',methods=['POST', 'GET'])
 def result():
